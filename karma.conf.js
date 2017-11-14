af4e67a88b189b96aa6a3a0aa6524fb010cff3c0
James Messinger <james.messinger@gmail.com>
2017-11-14 10:40:17 -0800

    Refactored the Karma test script

--- karma.conf.js
+++ karma.conf.js
@@ -1,83 +1,93 @@
 // Karma config
 // https://karma-runner.github.io/0.12/config/configuration-file.html
+
 'use strict';
 
-var baseConfig = {
-  frameworks: ['mocha', 'chai', 'sinon'],
-  reporters: ['verbose'],
-  files: [
-    // OmniPath
-    'dist/omnipath.min.js',
-    { pattern: 'dist/*.map', included: false, served: true },
+module.exports = function (karma) {
+  var config = {
+    frameworks: ['mocha', 'chai', 'sinon'],
+    reporters: ['verbose'],
 
-    // Test Fixtures
-    'test/fixtures/**/*.js',
+    files: [
+      // OmniPath
+      'dist/omnipath.min.js',
+      { pattern: 'dist/*.map', included: false, served: true },
 
-    // Tests
-    'test/specs/**/*.spec.js'
-  ]
+      // Test Fixtures
+      'test/fixtures/**/*.js',
+
+      // Tests
+      'test/specs/**/*.spec.js'
+    ]
+  };
+
+  exitIfDisabled();
+  configureCodeCoverage(config);
+  configureLocalBrowsers(config);
+  configureSauceLabs(config);
+
+  console.log('Karma Config:\n', JSON.stringify(config, null, 2));
+  karma.set(config);
 };
 
-module.exports = function (config) {
-  var karma = process.env.KARMA ? process.env.KARMA === 'true' : true;
-  var coverage = process.env.KARMA_COVERAGE ? process.env.KARMA_COVERAGE === 'true' : true;
-  var sauce = process.env.KARMA_SAUCE ? process.env.KARMA_SAUCE === 'true' : true;
-  var sauceUsername = process.env.SAUCE_USERNAME;
-  var sauceAccessKey = process.env.SAUCE_ACCESS_KEY;
+/**
+ * If this is a CI job, and Karma is not enabled, then exit.
+ * (useful for CI jobs that are only testing Node.js, not web browsers)
+ */
+function exitIfDisabled () {
+  var CI = process.env.CI === 'true';
+  var KARMA = process.env.KARMA === 'true';
 
-  if (!karma) {
-    // Karma is disabled, so abort immediately
+  if (CI && !KARMA) {
+    console.warn('Karma is not enabled');
     process.exit();
-    return;
   }
-
-  if (coverage) {
-    configureCodeCoverage(baseConfig);
-  }
-
-  if (sauce && sauceUsername && sauceAccessKey) {
-    configureSauceLabs(baseConfig);
-  }
-  else {
-    configureLocalBrowsers(baseConfig);
-  }
-
-  console.log('Karma Config:\n', JSON.stringify(baseConfig, null, 2));
-  config.set(baseConfig);
-};
+}
 
 /**
  * Configures the code-coverage reporter
  */
 function configureCodeCoverage (config) {
+  if (process.argv.indexOf('--cover') === -1) {
+    console.warn('Code-coverage is not enabled');
+    return;
+  }
+
   config.reporters.push('coverage');
-  config.files.splice(config.files.indexOf('dist/omnipath.min.js'), 1, 'dist/omnipath.test.js');
   config.coverageReporter = {
     reporters: [
       { type: 'text-summary' },
       { type: 'lcov' }
     ]
   };
+
+  config.files = config.files.map(function (file) {
+    if (typeof file === 'string') {
+      file = file.replace(/^dist\/(.*?)(\.min)?\.js$/, 'dist/$1.test.js');
+    }
+    return file;
+  });
 }
 
 /**
  * Configures the browsers for the current platform
  */
 function configureLocalBrowsers (config) {
-  var isMac = /^darwin/.test(process.platform),
-      isWindows = /^win/.test(process.platform),
-      isLinux = !(isMac || isWindows);
+  var isMac = /^darwin/.test(process.platform);
+  var isWindows = /^win/.test(process.platform);
+  var isLinux = !isMac && !isWindows;
 
   if (isMac) {
-    config.browsers = ['PhantomJS', 'Firefox', 'Chrome', 'Safari'];
+    config.browsers = ['Firefox', 'Chrome', 'Safari'];
   }
   else if (isLinux) {
-    config.browsers = ['PhantomJS', 'Firefox'];
+    config.browsers = ['Firefox', 'ChromeHeadless'];
   }
   else if (isWindows) {
-    config.browsers = ['PhantomJS', 'Firefox', 'Chrome', 'Safari', 'IE9', 'IE10', 'IE'];
+    config.browsers = ['Firefox', 'Chrome', 'IE9', 'IE10', 'IE'];
+
+    // NOTE: IE 6, 7, 8 are not supported by Chai
     config.customLaunchers = {
-      // NOTE: IE 6, 7, 8 are not supported by Chai
       IE9: {
         base: 'IE',
         'x-ua-compatible': 'IE=EmulateIE9'
@@ -95,88 +105,55 @@ function configureLocalBrowsers (config) {
  * https://github.com/karma-runner/karma-sauce-launcher
  */
 function configureSauceLabs (config) {
+  var SAUCE = process.env.SAUCE === 'true';
+  var username = process.env.SAUCE_USERNAME;
+  var accessKey = process.env.SAUCE_ACCESS_KEY;
+
+  if (!SAUCE || !username || !accessKey) {
+    console.warn('SauceLabs is not enabled');
+    return;
+  }
+
   var project = require('./package.json');
   var testName = project.name + ' v' + project.version;
   var build = testName + ' Build #' + process.env.TRAVIS_JOB_NUMBER + ' @ ' + new Date();
 
-  config.sauceLabs = {
-    build: build,
-    testName: testName,
-    tags: [project.name],
-    recordVideo: true,
-    recordScreenshots: true
-  };
-
-  config.customLaunchers = {
-    'IE-9': {
+  /* eslint camelcase:off */
+  var sauceLaunchers = {
+    SauceLabs_Chrome_Latest: {
+      base: 'SauceLabs',
+      platform: 'Windows 10',
+      browserName: 'chrome'
+    },
+    SauceLabs_Firefox_Latest: {
+      base: 'SauceLabs',
+      platform: 'Windows 10',
+      browserName: 'firefox'
+    },
+    SauceLabs_Safari_Latest: {
+      base: 'SauceLabs',
+      platform: 'macOS 10.12',
+      browserName: 'safari'
+    },
+    SauceLabs_IE_9: {
       base: 'SauceLabs',
       platform: 'Windows 7',
       browserName: 'internet explorer',
       version: '9'
     },
-    'IE-10': {
-      base: 'SauceLabs',
-      platform: 'Windows 7',
-      browserName: 'internet explorer',
-      version: '10'
-    },
-    'IE-11': {
-      base: 'SauceLabs',
-      platform: 'Windows 7',
-      browserName: 'internet explorer',
-      version: '11'
-    },
-    'IE-Edge': {
+    SauceLabs_IE_Edge: {
       base: 'SauceLabs',
       platform: 'Windows 10',
       browserName: 'microsoftedge'
     },
-    'Chrome-Latest': {
-      base: 'SauceLabs',
-      platform: 'Windows 7',
-      browserName: 'chrome'
-    },
-    'Firefox-Latest': {
-      base: 'SauceLabs',
-      platform: 'Windows 7',
-      browserName: 'firefox'
-    },
-    'Opera-Latest': {
-      base: 'SauceLabs',
-      platform: 'Windows 7',
-      browserName: 'opera'
-    },
-    'Safari-Latest': {
-      base: 'SauceLabs',
-      platform: 'OS X 10.10',
-      browserName: 'safari'
-    },
-    'iOS-6': {
-      base: 'SauceLabs',
-      platform: 'OS X 10.10',
-      browserName: 'iphone',
-      version: '6'
-    },
-    'iOS-8': {
-      base: 'SauceLabs',
-      platform: 'OS X 10.10',
-      browserName: 'iphone',
-      version: '8'
-    },
-    'Android-4-4': {
-      base: 'SauceLabs',
-      platform: 'Linux',
-      browserName: 'android',
-      version: '4.4'
-    },
-    'Android-5': {
-      base: 'SauceLabs',
-      platform: 'Linux',
-      browserName: 'android',
-      version: '5'
-    }
   };
 
   config.reporters.push('saucelabs');
-  config.browsers = Object.keys(config.customLaunchers);
+  config.browsers = Object.keys(sauceLaunchers);
+  config.customLaunchers = Object.assign(config.customLaunchers || {}, sauceLaunchers);
+  config.sauceLabs = {
+    build: build,
+    testName: testName,
+    tags: [project.name],
+  };
 }
